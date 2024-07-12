34248bd62898d91757426d19335867261774f1cd
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-07-12 14:26:52 -0300

    feat: add save/load functionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Game
   def initialize
     @word = ''
@@ -13,6 +15,25 @@ class Game
 
   attr_accessor :word, :word_array, :underscore_array, :incorrect_guesses, :incorrect_letters
 
+  def check_saves
+    if Dir.exist?('saves') && !Dir.entries('saves').empty?
+      load_save = ''
+      until %w[y n].include?(load_save)
+        puts "Do you want to load a save? [y/n]\n"
+
+        load_save = gets.chomp
+        puts "\n"
+      end
+
+      if load_save == 'y'
+        load_game
+        return
+      end
+    end
+
+    select_word
+  end
+
   def select_word
     dictionary = File.read('google-10000-english-no-swears.txt').split("\n")
 
@@ -23,7 +44,7 @@ class Game
   end
 
   def display_word_and_guesses(incorrect_guesses, incorrect_letters, underscore_array)
-    puts "-----------------------\n\n"
+    puts "-----------------------------------\n\n"
 
     puts "Incorrect guesses left:\n#{incorrect_guesses}"
     puts "Incorrect letters:\n#{incorrect_letters.join(' ')}" unless incorrect_letters.empty?
@@ -32,7 +53,9 @@ class Game
   end
 
   def check_guess(guess)
-    if word.include?(guess) && !underscore_array.include?(guess)
+    if guess == 'save'
+      save_game
+    elsif word.include?(guess) && !underscore_array.include?(guess)
       word_array.each_with_index do |value, index|
         underscore_array[index] = guess if value == guess
       end
@@ -61,17 +84,59 @@ class Game
 
   public
 
-  def start_game
-    select_word
+  def save_game
+    Dir.mkdir('saves') unless Dir.exist?('saves')
 
+    puts "Please, choose your save file name:\n\n"
+    print '> '
+    filename = gets.chomp
+
+    File.open("saves/#{filename}.yaml", 'w') do |file|
+      file.puts YAML.dump({
+                            word: word,
+                            word_array: word_array,
+                            underscore_array: underscore_array,
+                            incorrect_guesses: incorrect_guesses,
+                            incorrect_letters: incorrect_letters
+                          })
+    end
+  end
+
+  def load_game
+    saves = Dir.entries('saves').reject { |entry| ['.', '..'].include?(entry) }
+    saves.map! { |save| save.gsub('.yaml', '') }
+
+    puts "Please, choose a save file to load:\n\n"
+    saves.each_with_index do |save, index|
+      puts "#{index + 1}: #{save}"
+    end
+
+    selection = ''
+    until (0..(saves.length - 1)).include?(selection)
+      print "\n> "
+
+      selection = gets.chomp.to_i - 1
+    end
+
+    data = YAML.load File.read("saves/#{saves[selection]}.yaml")
+
+    self.word = data[:word]
+    self.word_array = data[:word_array]
+    self.underscore_array = data[:underscore_array]
+    self.incorrect_guesses = data[:incorrect_guesses]
+    self.incorrect_letters = data[:incorrect_letters]
+  end
+
+  def start_game
+    check_saves
     game_over = false
 
     until game_over
       display_word_and_guesses(incorrect_guesses, incorrect_letters, underscore_array)
 
       guess = ''
-      until ('a'..'z').include?(guess)
-        print 'Please, make a guess: '
+      until guess == 'save' || ('a'..'z').include?(guess)
+        print "Please, make a guess or type 'save'\nto save the game: "
         guess = gets.chomp.downcase
         puts "\n"
       end
